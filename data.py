caf0bf74d23227f23f4059112676df9a378b77bc
shanekao <shanekao19870615@gmail.com>
2021-04-05 20:34:15 +0800

    read data

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import os
 from datetime import datetime, timedelta
 
+import pandas as pd
+
 from configs import DATA_DIR
 
 
@@ -16,24 +18,34 @@ class Data:
             file_date = datetime.strptime(i.split('.')[0], "%Y%m%d")
             date_diff = current_date - file_date
             if date_diff > timedelta(days=self._DAYS):
-
                 break
             else:
                 yield os.path.join(DATA_DIR, self.alliance, i)
                 current_date = file_date
 
     @property
-    def data_path(self):
+    def path(self):
         return os.path.join(DATA_DIR, self.alliance)
 
     @property
     def files(self):
-        files_ = os.listdir(self.data_path)
+        files_ = os.listdir(self.path)
         files_ = sorted(files_, key=lambda x: datetime.strptime(x.split('.')[0], "%Y%m%d"), reverse=True)
         return files_
 
+    @property
+    def raw(self):
+        # TODO add date to game_time column
+        data_ = []
+        for file in self:
+            df_ = pd.read_csv(file, index_col=None, header=0)
+            data_.append(df_)
+        df = pd.concat(data_, axis=0, ignore_index=True)
+        return df
+
+
 
 if __name__ == "__main__":
-    data = Data(alliance="NBA")
-    for file in data:
-        print(file)
+    data = Data(alliance="P+")
+    df = data.raw
+    print(df)
