982701d5ac0088b6b70dc90f8c6d84cc16de9c73
shanekao <shanekao19870615@gmail.com>
2021-04-06 20:44:22 +0800

    1. game_date to datetime type 2. add incoming

--- data.py
+++ data.py
@@ -1,9 +1,13 @@
 import os
+from pathlib import Path
 from datetime import datetime, timedelta
 
 import pandas as pd
 
 from configs import DATA_DIR
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 
 class Data:
@@ -35,17 +39,25 @@ class Data:
 
     @property
     def raw(self):
-        # TODO add date to game_time column
         data_ = []
         for file in self:
+            date_ = os.path.splitext(Path(file).name)[0]
             df_ = pd.read_csv(file, index_col=None, header=0)
+            df_["game_time"] = df_["game_time"].apply(
+                lambda x: datetime.strptime(date_ + x, "%Y%m%d%p %I:%M")
+            )
             data_.append(df_)
         df = pd.concat(data_, axis=0, ignore_index=True)
+        df = df.sort_values(by="game_time", ignore_index=True)
         return df
 
+    @property
+    def incoming(self):
+        df = self.raw
+        return df[df["game_time"] > datetime.now()]
 
 
 if __name__ == "__main__":
-    data = Data(alliance="P+")
-    df = data.raw
+    data = Data(alliance="中華職棒")
+    df = data.incoming
     print(df)
