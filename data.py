30eaaf78b6ff46659e299c8c25d48438828e2580
shanekao <shanekao19870615@gmail.com>
2021-04-07 21:58:05 +0800

    add get training

--- data.py
+++ data.py
@@ -12,6 +12,12 @@ pd.set_option('display.max_columns', None)
 
 class Data:
     _DAYS = 90
+    _BASE_COLUMNS = ["game_time", "away_score", "home_score", "away_team", "home_team"]
+    _TW_DIFF_COLUMNS = ['tw_diff', 'tw_diff_away_odds', 'tw_diff_home_odds', 'tw_diff_home_count', 'tw_away_odds',
+                        'tw_home_odds', 'tw_home_count']
+    _TW_TOTAL_COLUMNS = ['tw_total', 'tw_under_odds', 'tw_over_odds', 'tw_over_count']
+    _OVERSEA_DIFF_COLUMNS = ['oversea_diff', 'oversea_diff_home_count']
+    _OVERSEA_TOTAL_COLUMNS = ['oversea_total', 'oversea_over_count']
 
     def __init__(self, alliance):
         self.alliance = alliance
@@ -56,8 +62,28 @@ class Data:
         df = self.raw
         return df[df["game_time"] > datetime.now()]
 
+    @property
+    def history(self):
+        df = self.raw
+        df = df[df['away_score'].notnull() & df['home_score'].notnull()]
+        return df
+
+    def get_train(self, book_maker, type_of_bet):
+        assert book_maker == "tw" or book_maker == "oversea"
+        assert type_of_bet == "total" or type_of_bet == "diff"
+        extra_cols = getattr(self, '_'.join(["", book_maker.upper(), type_of_bet.upper(), "COLUMNS"]))
+        df = self.history[self._BASE_COLUMNS + extra_cols]
+        df = df[df[extra_cols[0]].notnull()]
+        return df
+
 
 if __name__ == "__main__":
-    data = Data(alliance="中華職棒")
-    df = data.incoming
-    print(df)
+    data = Data(alliance="NBA")
+    df1 = data.get_train(book_maker="tw", type_of_bet="diff")
+    print(df1)
+    df2 = data.get_train(book_maker="tw", type_of_bet="total")
+    print(df2)
+    df3 = data.get_train(book_maker="oversea", type_of_bet="diff")
+    print(df3)
+    df4 = data.get_train(book_maker="oversea", type_of_bet="total")
+    print(df4)
